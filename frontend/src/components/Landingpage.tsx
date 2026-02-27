6bac84d76083b1f34c66690e4999c69f21deffa7
Saud12131 <saudsayyedofficial@gmail.com>
2026-02-27 06:27:39 +0530

    added health endpoin

--- frontend/src/components/Landingpage.tsx
+++ frontend/src/components/Landingpage.tsx
@@ -1,9 +1,21 @@
+"use client";
+import { useEffect } from "react";
 import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
 import HowItWorksSection from "./HowItWorks";
 
 export default function LandingPage() {
+  useEffect(() => {
+    // Wake up the backend server
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/`, {
+      method: 'GET',
+      mode: 'no-cors' // Avoid CORS issues for the wake-up call
+    }).catch(() => {
+      // Silently ignore errors - this is just for waking up the backend
+    });
+  }, []);
+
   return (
     <div className="bg-black min-h-screen selection:bg-white selection:text-black">
       <Navbar />
