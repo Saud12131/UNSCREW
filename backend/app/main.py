e589682e5e56b4b1d7137de3eab6e3b17dc4e724
Saud12131 <saudsayyedofficial@gmail.com>
2026-02-28 15:57:44 +0530

    added frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # your frontend URL
+    allow_origins=["https://unscrew-eta.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
